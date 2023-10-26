40b264c788f74e2f1accfb8523acbf93a921271d
JPscioli <greatdesignersoftware@outlook.com>
2023-10-26 08:02:50 -0300

    feat:'capitulo 03 Completo'

--- Spring_in_Action/taco-cloud/src/main/java/tacos/Taco.java
+++ Spring_in_Action/taco-cloud/src/main/java/tacos/Taco.java
@@ -1,19 +1,20 @@
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.Generated;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt = new Date();
@@ -22,8 +23,11 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
+    @ManyToMany
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
