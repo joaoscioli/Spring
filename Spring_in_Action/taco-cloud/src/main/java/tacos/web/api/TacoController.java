7fd8a68a76280cfd669737056908e33007f5b3bf
JPscioli <greatdesignersoftware@outlook.com>
2023-11-13 13:41:19 -0300

    feat:'capitulo 07 completo'

--- Spring_in_Action/taco-cloud/src/main/java/tacos/web/api/TacoController.java
+++ Spring_in_Action/taco-cloud/src/main/java/tacos/web/api/TacoController.java
@@ -2,12 +2,13 @@ package tacos.web.api;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tacos.Taco;
 import tacos.data.TacoRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos",
@@ -28,4 +29,26 @@ public class TacoController {
         return tacoRepo.findAll(page).getContent();
     }
 
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Taco> tacoById(@PathVariable("id") Long id) {
+        return tacoRepo.findById(id);
+    }
+
+    /*
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+    */
 }
